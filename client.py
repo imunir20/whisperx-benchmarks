54a63cbf9377b608274d97de2ef7d3ae30c3875e
Ibrahim Munir <ibrahimmunir13@gmail.com>
2025-01-05 09:33:16 -0500

    Add files via upload

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import requests
+import time
 from concurrent.futures import ThreadPoolExecutor
 
 # URL of the server's transcribe endpoint
@@ -18,12 +19,18 @@ def send_request():
 
 def main():
     # Number of concurrent requests
-    num_requests = 3
+    num_requests = 25
 
+    print(f"Executing {num_requests} requests")
+    start_time = time.time()
+    print(f"Starting time: {start_time}")
     with ThreadPoolExecutor(max_workers=num_requests) as executor:
         futures = [executor.submit(send_request) for _ in range(num_requests)]
         for future in futures:
             future.result()
+    end_time = time.time()
+    print(f"Time taken: {end_time - start_time:.2f} seconds")
+    print(f"Difference: {end_time - start_time:.2f} seconds")
 
 if __name__ == '__main__':
     main()
